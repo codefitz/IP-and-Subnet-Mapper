ef62fef8bf576e484881a41326be4152f0bc03a4
Wes Moskal-Fitzpatrick <github@fitzmoskal.me>
2021-07-23 20:10:59 +0100

    v0.1.2
    - Proper versioning
    - Refactored code to take multiple inputs
    - Output to optional CSV or CLI
    - Removed CSV files

--- README.md
+++ README.md
@@ -1,5 +1,100 @@
-# IP and Subnet Mapper
+# IP and Subnet Mapper v0.1.2
 
-Takes 2 CSV file inputs; one with a list of IP addresses, and one with a list of Subnets + Labels and will map them to one file.
+Check for an IP address within a given Subnet.
 
-Example ips.csv and subnets.csv included.
+## Input Files
+
+| Flag | File | Format | Column 1 | Column 2 |
+| :--: | ---- | ------ | -------- | -------- |
+| -I   | IP Addresses | CSV | IP Address/CIDR Range | Network Name |
+| -S   | Subnets      | CSV | Subnet CIDR           | Subnet Name  |
+
+## Example Usage
+
+### Help
+
+```shell
+$ python3 ipcheck.py -h
+usage: ipcheck.py [-h] [-s <subnet>] [-S <filename>] [-i <ip address>] [-I <filename>] [-o <filename>]
+
+IP Subnet Checker
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s <subnet>, --subnet <subnet>
+                        A subnet to check against.
+
+  -S <filename>, --subnets_file <filename>
+                        CSV file containing a list of subnets to check against.
+
+                        Format - IPAddress, Name
+
+  -i <ip address>, --ip_address <ip address>
+                        IP address to check in subnet.
+
+  -I <filename>, --ip_file <filename>
+                        CSV file containing IP addresses to check in subnet.
+
+                        Format - Subnet, Name
+
+  -o <filename>, --output_file <filename>
+                        Specify a CSV output_file file.
+```
+
+### Check a Single IP against a Single Subnet
+
+```shell
+$ python3 ipcheck.py -i 192.168.1.210 -s 192.168.1.0/24
+IP Address 192.168.1.210 found in 192.168.1.0/24 ( Inputted Address: 192.168.1.210 Inputted Subnet: 192.168.1.0/24 )
+1 IP addresses processed.
+```
+
+### Input a list of IPs
+
+```console
+$ cat input.csv
+192.168.1.0,Test1
+192.168.1.1,Test2
+192.168.1.2,Test3
+192.168.1.136,Test1
+192.168.1.176,Test2
+
+$ python3 ipcheck.py -I input.csv -s 192.168.1.128/25
+IP Address 192.168.1.0 NOT in 192.168.1.128/25 ( Inputted Address: 192.168.1.0 Inputted Subnet: 192.168.1.128/25 )
+IP Address 192.168.1.1 NOT in 192.168.1.128/25 ( Inputted Address: 192.168.1.1 Inputted Subnet: 192.168.1.128/25 )
+IP Address 192.168.1.2 NOT in 192.168.1.128/25 ( Inputted Address: 192.168.1.2 Inputted Subnet: 192.168.1.128/25 )
+IP Address 192.168.1.136 found in 192.168.1.128/25 ( Inputted Address: 192.168.1.136 Inputted Subnet: 192.168.1.128/25 )
+IP Address 192.168.1.176 found in 192.168.1.128/25 ( Inputted Address: 192.168.1.176 Inputted Subnet: 192.168.1.128/25 )
+5 IP addresses processed.
+```
+
+### Output to a CSV File
+
+```console
+$ cat subnets.csv
+192.168.1.128/25,TestSub
+
+$ python3 ipcheck.py -i 192.168.1.210 -S subnets.csv -o output.csv
+1 IP addresses processed.
+
+Results saved to output.csv
+
+$ cat output.csv
+IP Address,IP Name,Subnet,Subnet Name,Inputted Address,Inputted Subnet
+192.168.1.210,,192.168.1.128/25,TestSub,192.168.1.210,192.168.1.128/25
+```
+
+### Works on a Single IP
+
+```console
+$ python3 ipcheck.py -i 192.168.1.210 -s 192.168.1.0
+IP Address 192.168.1.210 NOT in 192.168.1.0/32 ( Inputted Address: 192.168.1.210 Inputted Subnet: 192.168.1.0 )
+1 IP addresses processed.
+```
+
+### Works in Reverse
+
+```console
+$ python3 ipcheck.py -i 192.168.1.208/30 -s 192.168.1.210
+
+```
--- ipcheck.py
+++ ipcheck.py
@@ -5,39 +5,132 @@
 
 import ipaddress
 import csv
+import argparse
+from argparse import RawTextHelpFormatter
+from cidrize import cidrize
 
-ipsub = {}
+parser = argparse.ArgumentParser(description='IP Subnet Checker',formatter_class=RawTextHelpFormatter)
+parser.add_argument('-s', '--subnet', dest='subnet',  type=str, required=False, help='A subnet to check against.\n\n', metavar='<subnet>')
+parser.add_argument('-S', '--subnets_file', dest='subnets_file',  type=str, required=False, help='CSV file containing a list of subnets to check against.\n\nFormat - IPAddress, Name\n\n', metavar='<filename>')
+parser.add_argument('-i', '--ip_address', dest='ip_address',  type=str, required=False, help='IP address to check in subnet.\n\n', metavar='<ip address>')
+parser.add_argument('-I', '--ip_file', dest='ip_file',  type=str, required=False, help='CSV file containing IP addresses to check in subnet.\n\nFormat - Subnet, Name\n\n', metavar='<filename>')
+parser.add_argument('-o', '--output_file', dest='output_file',  type=str, required=False, help='Specify a CSV output_file file.\n\n', metavar='<filename>')
 
-with open('subnets.csv') as subfile:
-    reader = csv.reader(subfile)
-    for line in reader:
-        ipsub[line[0]] = line[1]
+args = parser.parse_args()
+subnet = args.subnet
+subnets_file = args.subnets_file
+ip_addr = args.ip_address
+ip_file = args.ip_file
+output_file = args.output_file
 
-iplist = []
-count = 0
+def range_to_ips(iprange):
+    list_of_ips = []
+    if iprange:
+        try:
+            ipaddr = ipaddress.ip_address(iprange)
+            list_of_ips.append(ipaddr)
+        except:
+            try:
+                ipaddrs = ipaddress.ip_network(iprange)
+                for ipaddr in ipaddrs:
+                    list_of_ips.append(ipaddr)
+            except:
+                try:
+                    ipaddrs = ipaddress.ip_network(iprange,strict=False)
+                    msg = 'Address %s is not valid CIDR syntax, using recommended CIDR: %s' % (iprange, ipaddrs)
+                    print(msg)
+                    for ipaddr in ipaddrs:
+                        list_of_ips.append(ipaddr)
+                except:
+                    try:
+                        cidrip = cidrize(iprange)
+                        for cidr in cidrip:
+                            ipaddrs = ipaddress.ip_network(cidr)
+                            for ipaddr in ipaddrs:
+                                list_of_ips.append(ipaddr)
+                    except:
+                        msg = 'Address %s is not valid CIDR syntax, cannot process!' % (iprange)
+                        print(msg)
+    return list_of_ips
 
-with open('ips.csv') as ipfile:
-    reader = csv.reader(ipfile)
-    for line in reader:
+def validate_subnet(subnet):
+    if subnet:
+        try:
+            ip = ipaddress.ip_address(subnet)
+            subbed = ipaddress.ip_network(ip)
+            return subbed
+        except:
+            try:
+                subbed = ipaddress.ip_network(subnet)
+                return subbed
+            except:
+                try:
+                    subbed = ipaddress.ip_network(subnet,strict=False)
+                    msg = 'Address %s is not valid CIDR syntax, using recommended CIDR: %s' % (subnet, subbed)
+                    print(msg)
+                    return subbed
+                except:
+                    try:
+                        cidrip = cidrize(subnet)
+                        subbed = ipaddress.ip_network(cidrip)
+                        return subbed
+                    except:
+                        msg = 'Subnet %s is not valid CIDR syntax, cannot process!' % (subnet)
+                        print(msg)
+
+def output(ip,ip_name,valid_subnet,subnet,sub_name):
+    if valid_subnet:
+        if ip in valid_subnet:
+            values = [str(ip),ip_name,str(valid_subnet),sub_name,ip_addr,subnet]
+            msg = ("IP Address",values[0],"found in",values[2],"( Inputted Address:",values[4],"Inputted Subnet:",values[5],")")
+        else:
+            values = [str(ip),ip_name,str(valid_subnet),sub_name,ip_addr,subnet]
+            msg = ("IP Address",values[0],"NOT in",values[2],"( Inputted Address:",values[4],"Inputted Subnet:",values[5],")")
+        if output_file:
+            mapped.append(values)
+        else:
+            print(" ".join(msg))
+            mapped.append(values)
+    return mapped
+
+def checkIPs(ip_addr,ip_name=None):
+    count = 0
+    ip_list = range_to_ips(ip_addr)
+    for ip in ip_list:
         count += 1
-        iplist.append(line[0])
+        if subnet:
+            validated = validate_subnet(subnet)
+            mapped = output(ip,ip_name,validated,subnet,None)
+        elif subnets_file:
+            with open(subnets_file) as file:
+                reader = csv.reader(file)
+                for line in reader:
+                    subnet_ip = line[0]
+                    sub_name = line[1]
+                    validated = validate_subnet(subnet_ip)
+                    mapped = output(ip,ip_name,validated,subnet_ip,sub_name)
+    mapped.sort( key = lambda k: ipaddress.ip_address(k[0]) )
+    return mapped
 
 mapped = []
-exists = []
 
+if ip_addr:
+    mapped = checkIPs(ip_addr)
+    mapped.insert(0, [ "IP Address", "IP Name", "Subnet", "Subnet Name", "Inputted Address", "Inputted Subnet" ])
+elif ip_file:
+    with open(ip_file) as file:
+        reader = csv.reader(file)
+        count = 0
+        for line in reader:
+            ip_addr = line[0]
+            ip_name = line[1]
+            mapped = checkIPs(ip_addr,ip_name)
+        mapped.insert(0, [ "IP Address", "IP Name", "Subnet", "Subnet Name", "Inputted Address", "Inputted Subnet" ])
 
-for sub in ipsub:
-    for ip in iplist:
-        if ipaddress.ip_address(ip) in ipaddress.ip_network(sub, False):
-            mapped.append([ip,sub,ipsub[sub]])
-            exists.append(ip)
+print (len(mapped)-1,"IP addresses processed.\n")
 
-for ip in iplist:
-    if ip not in exists:
-        mapped.append([ip,"None","None"])
-
-mapped.sort()
-with open('ips_mapped.csv', 'w') as file:
-    file.writelines(','.join(i) + '\n' for i in mapped)
-
-print ( "Done. " + str(count) + " IP addresses processed.")
+if output_file:
+    with open(output_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(mapped)
+    print("Results saved to",output_file,"\n")
